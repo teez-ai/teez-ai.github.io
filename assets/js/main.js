6f945be84913008668fd1c8a00e585281924094a
teez-ai <68920590+teez-ai@users.noreply.github.com>
2023-06-19 14:22:08 -0700

    Added new files

---
+++ assets/js/main.js
@@ -0,0 +1,86 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction=()=>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(l => l.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                     : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SERVICES MODAL ===============*/
+
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.projects__container', {
+    selectors: {
+        target: '.projects__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+const linkWork=document.querySelectorAll('.projects__item')
+
+function activeWork(){
+    linkWork.forEach(l=>l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+linkWork.forEach(l=>l.addEventListener('click',activeWork))
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections=document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY=window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight=current.offsetHeight,
+              sectionTop=current.offsetTop-68,
+              sectionId=current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*='+sectionId+']').classList.add('active-link')
+        }else
+        {
+            document.querySelector('.nav__menu a[href*='+sectionId+']').classList.remove('active-link')
+        }
+    })       
+}
+window.addEventListener('scroll',scrollActive)
+
+/*=============== LIGHT DARK THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
